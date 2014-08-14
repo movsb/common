dca684bd88efe076c3926f44aba2a3c54c4f1bf1
movsb <anhbk@qq.com>
2014-08-14 10:17:45 +0800

    删除一些暂时不再需要的文件

--- sources/cmd_dlg.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CMD_DLG_H__
-#define __CMD_DLG_H__
-
-#include <Windows.h>
-#include <WindowsX.h>
-
-int showCmd(char* file);
-
-#endif//!__CMD_DLG_H__
--- sources/layout/SdkLayout.h
+++
@@ -1,4 +0,0 @@
-void* NewLayout(HWND hWnd, HINSTANCE hInst, LPCTSTR xml);
-void DeleteLayout(void* ptr);
-void SizeLayout(void* ptr, const SIZE* sz);
-void VisibaleLayout(void* ptr, LPCTSTR name, BOOL bVisible);
