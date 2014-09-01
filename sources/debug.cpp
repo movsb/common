1d45f1652684d991e50ee02465a1c00277533157
movsb <anhbk@qq.com>
2014-09-01 20:45:48 +0800

    加入智能断言/智能确保

--- sources/debug.cpp
+++ sources/debug.cpp
@@ -1,32 +1,8 @@
 #include <stdio.h>
 #include <windows.h>
+#include <fstream>
 
 #include "debug.h"
-
-/*
-void DebugOut(char* fmt, ...)
-{	
-	char buf[1024] = {0};
-	va_list va;
-	COPYDATASTRUCT cds = {0};
-	int len;
-
-	HWND hWndDebugWindow = NULL;
-	hWndDebugWindow = FindWindow("nbsg_debug_window_class", NULL);
-	if(!hWndDebugWindow || !IsWindow(hWndDebugWindow))
-		return;
-
-	va_start(va, fmt);
-	//vsprintf(buf, fmt, va);
-	len=_vsnprintf(buf,sizeof(buf),fmt,va);
-	
-	cds.dwData = 0;
-	cds.lpData = (void*)buf;
-	cds.cbData = len+1;
-	SendMessage(hWndDebugWindow, WM_COPYDATA, 0, (LPARAM)&cds);
-	return;
-}*/
-
 
 void DebugOut(char* fmt, ...)
 {	
@@ -39,3 +15,4 @@ void DebugOut(char* fmt, ...)
 	WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE),buf,len,(LPDWORD)&dw,NULL);
 	return;
 }
+
--- sources/debug.h
+++ sources/debug.h
@@ -1,5 +1,4 @@
-#ifndef __DEBUGOUT_H__
-#define __DEBUGOUT_H__
+#pragma once
 
 #ifdef _DEBUG
 #define debug_out(x) DebugOut##x
@@ -9,4 +8,91 @@
 
 void DebugOut(char* fmt, ...);
 
-#endif//!DEBUGOUT_INC_
+
+//////////////////////////////////////////////////////////////////////////
+// Smart Assert
+
+// #ifdef _DEBUG
+// #define __SMART_DEBUG
+// #else
+// #undef __SMART_DEBUG
+// #endif
+
+#define __SMART_DEBUG
+
+#ifdef __SMART_DEBUG
+
+extern std::fstream __debug_file;
+
+class CSmartAssert
+{
+public:
+	CSmartAssert()
+		: __SMART_ASSERT_A(*this)
+		, __SMART_ASSERT_B(*this)
+	{}
+	CSmartAssert& __SMART_ASSERT_A;
+	CSmartAssert& __SMART_ASSERT_B;
+	CSmartAssert& Context(const char* expr, const char* file, int line)
+	{
+		m_expr = expr;
+		__debug_file << "\r\n[" << file << ", " << line << "]\r\n";
+		__debug_file << "Assertion failed: " << expr << "\r\n";
+		__debug_file.flush();
+		return *this;
+	}
+
+	// to use this , you must have an operator<<(ostream&, your_class) overloaded
+	template <class T>
+	CSmartAssert& Out(const char* str, T t)
+	{
+		__debug_file << "\t" << str << " = " << t << "\r\n";
+		__debug_file.flush();
+		return *this;
+	}
+	void Warning()
+	{
+		std::string str("警告: 应用程序遇到警告性错误!\n\n");
+		str += m_expr;
+		str += "\n\n请查看debug.txt调试输出!";
+		::MessageBox(nullptr, str.c_str(), nullptr, MB_ICONWARNING);
+	}
+	void Fatal()
+	{
+		std::string str("错误: 应用程序遇到严重性错误!\n\n");
+		str += m_expr;
+		str += "\n\n请查看debug.txt调试输出!";
+		::MessageBox(nullptr, str.c_str(), nullptr, MB_ICONERROR);
+	}
+	void Stop()
+	{
+		std::string str("停止: 应用程序错误!\n\n请立即取消当前操作! 否则可能造成不可想像的后果!\n\n");
+		str += m_expr;
+		str += "\n\n请查看debug.txt调试输出!";
+		::MessageBox(nullptr, str.c_str(), nullptr, MB_ICONEXCLAMATION);
+	}
+
+private:
+	std::string m_expr;
+};
+
+#define __SMART_ASSERT_A(x) __SMART_ASSERT(x,B)
+#define __SMART_ASSERT_B(x) __SMART_ASSERT(x,A)
+
+#define __SMART_ASSERT(x,next) \
+	__SMART_ASSERT_A.Out(#x, x).__SMART_ASSERT_##next
+
+#define SMART_ASSERT(expr) \
+	if(expr) ;\
+	else CSmartAssert().Context(#expr, __FILE__, __LINE__).__SMART_ASSERT_A
+
+#define SMART_ENSURE(a,b) SMART_ASSERT((a)b) // or (a) ## b
+
+#else // !SMART_DEBUG
+
+#define SMART_ASSERT __SMART_ASSERT/
+#define __SMART_ASSERT /
+
+#define SMART_ENSURE(a,b) a;SMART_ASSERT
+
+#endif // SMART_DEBUG
