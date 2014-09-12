ad3a77f39f7057a2628e888bf48f29a32ced1c16
movsb <anhbk@qq.com>
2014-09-13 01:40:13 +0800

    整理资源文件

--- res/resource.h
+++ res/resource.h
@@ -4,27 +4,22 @@
 //
 #define IDD_DLG_MAIN                    101
 #define IDI_ICON1                       106
-#define IDD_MSG                         107
-#define IDD_DLG_TEMPLATE                109
 #define IDR_MENU_OTHER                  111
 #define IDD_DLG_TIMEOUTS                114
 #define IDR_MENU_MORE                   116
 #define IDD_DLG_PINCTRL                 117
-#define IDD_DLG_EXPR                    118
-#define IDR_MENU_EDIT_RECV              120
-#define IDD_DLG_STR2HEX                 122
-#define IDD_DLG_MONITOR                 125
-#define IDD_CMD                         127
 #define IDD_SEND_CMD                    128
-#define IDD_CMD_TEMPLATE                129
 #define IDR_ACCELERATOR1                130
 #define IDD_DIALOG1                     131
 #define IDD_GETBR                       131
 #define IDR_101                         134
 #define IDR_102                         135
 #define IDR_RCDATA2                     137
+#define MENU_EDIT_CONTEXTMENU           138
+#define MENU_RICHEDIT_CONTEXTMENU       138
+#define IDD_DIALOG2                     139
+#define IDD_TEMPLATE                    139
 #define IDC_BTN_SEND                    1000
-#define IDC_MSG_OK                      1001
 #define IDC_CBO_CP                      1002
 #define IDC_CBO_BR                      1005
 #define IDC_CBO_CHK                     1006
@@ -46,16 +41,11 @@
 #define IDC_BTN_CLR_COUNTER             1022
 #define IDC_BTN_HELP                    1023
 #define IDC_BTN_COPY_RECV               1024
-#define IDC_MSG_EDIT_MSG                1025
 #define IDC_BTN_COPY_SEND               1026
 #define IDC_BTN_SAVEFILE                1027
-#define IDC_MSG_HOTLINK                 1028
 #define IDC_BTN_STOPDISP                1029
 #define IDC_BTN_LOADFILE                1030
-#define IDC_HOOK_COMBO                  1031
-#define IDC_HOOK_STATIC                 1032
 #define IDC_EDIT_RECV2                  1033
-#define IDC_STATIC_VERSION              1034
 #define IDC_BTN_MORE_SETTINGS           1035
 #define IDC_CTO_EDIT_READ_INTERVAL      1036
 #define IDC_CTO_EDIT_READ_MULTIPLIER    1037
@@ -68,28 +58,18 @@
 #define IDC_PINCTRL_OK                  1044
 #define IDC_CBO_PINCTRL_DTR             1045
 #define IDC_CBO_PINCTRL_RTS             1046
-#define IDC_EXPR_EDIT_EXPR              1047
-#define IDC_EXPR_EDIT_RESULT            1048
 #define IDC_STATIC_RECV                 1049
 #define IDC_STATIC_TIMER                1050
-#define IDC_EDIT_STR2HEX_SRC            1051
-#define IDC_EDIT_STR2HEX_DST            1052
-#define IDC_EDIT_MONITOR_RECV           1053
 #define IDC_STATIC_PICTURE              1054
 #define IDC_STATIC_SEND                 1056
-#define IDC_CMD_CMDLIST                 1057
-#define IDC_CMD_DATA                    1058
-#define IDC_CMD_SEND                    1059
-#define IDC_CHECK_IGNORE_RETURN         1060
-#define IDC_CHECK_USE_ESCAPE_CHAR       1061
 #define IDC_CMD_STATIC_TYPE             1062
 #define IDC_CMD_STATIC_SIZE             1064
 #define IDC_CMD_EDIT_CMD                1065
 #define IDC_CMD_BUTTON_SEND             1066
 #define IDC_CMD_STATIC_NAME             1067
 #define IDC_BUTTON1                     1068
+#define IDC_BTN_SEND_FMT_CONFIG         1068
 #define IDC_BUTTON2                     1069
-#define IDC_EDIT1                       1070
 #define MAINSTATIC_CP                   1071
 #define MAINSTATIC_BR                   1072
 #define MAINSTATIC_CHK                  1073
@@ -111,7 +91,6 @@
 #define MENU_MORE_PINCTRL               40007
 #define MENU_OTHER_DEVICEMGR            40008
 #define ID_POPUPMENU_40009              40009
-#define MENU_EDIT_CHINESE               40010
 #define ID_POPUPMENU_40011              40011
 #define MENU_OTHER_STR2HEX              40012
 #define ID_POPUPMENU_40013              40013
@@ -126,7 +105,6 @@
 #define IDACC_CLRCOUNTER                40024
 #define ID_POPUPMENU_40028              40028
 #define ID_POPUPMENU_40030              40030
-#define MENU_EDIT_SEND_INPUT_CHAR       40031
 #define ID_POPUPMENU_40032              40032
 #define ID_EDITCONTEXTMENU_COPY         40039
 #define ID_EDITCONTEXTMENU_CUT          40040
@@ -134,15 +112,14 @@
 #define ID_EDITCONTEXTMENU_DELETE       40042
 #define ID_EDITCONTEXTMENU_SELALL       40043
 #define ID_EDITCONTEXTMENU_FULLSCREEN   40044
-#define MENU_EDIT_EMPTY                 40033
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        138
-#define _APS_NEXT_COMMAND_VALUE         40034
-#define _APS_NEXT_CONTROL_VALUE         1088
+#define _APS_NEXT_RESOURCE_VALUE        140
+#define _APS_NEXT_COMMAND_VALUE         40046
+#define _APS_NEXT_CONTROL_VALUE         1090
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
