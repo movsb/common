cce7eda2eb90a100ee244e1f8a3dea9f7a105491
movsb <anhbk@qq.com>
2014-09-18 14:01:28 +0800

    更新预编译头和资源文件

--- sources/stdafx.h
+++ sources/stdafx.h
@@ -2,12 +2,56 @@
 #ifndef __stdafx_120937012387901247_h__
 #define __stdafx_120937012387901247_h__
 
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <process.h>
+
 #include <windows.h>
 #include <Richedit.h>
-#include <assert.h>
+#include <WindowsX.h>
+#include <Dbt.h>
+#include <tchar.h>
+#include <SetupAPI.h>
+#include <devguid.h>
+#include <WinInet.h>
 
+#include "Sdklayout/SdkLayout.h"
+
+
+#include "debug.h"
+#include "utils.h"
+#include "struct/list.h"
 #include "struct/Config.h"
+#include "struct/memory.h"
+#include "struct/Thunk.h"
 
-extern comconfig* g_cfg;
+#include "Windows/Timer.h"
+#include "Windows/theApp.h"
+#include "Windows/WindowManager.h"
+#include "Windows/Wnd.h"
+#include "Windows/TextEditor.h"
+#include "Windows/FileDialog.h"
+#include "Windows/DialogBuilder.h"
+#include "Windows/FileOperation.h"
+#include "Windows/InputBox.h"
+
+#include "DataProcessor.h"
+#include "str2hex.h"
+#include "asctable.h"
+#include "comm.h"
+#include "about.h"
+#include "msg.h"
+
+extern Common::CComConfig* comcfg;
 
 #endif//!__stdafx_h__
