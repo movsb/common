c9f55cb0fffec7ab6b36247bd98618e408ae1e8e
movsb <anhbk@qq.com>
2016-08-31 01:56:58 +0800

    修正测试代码串口类跨线程操作IO问题

--- src/test.cpp
+++ src/test.cpp
@@ -9,39 +9,24 @@ using namespace Common;
 class CommandNotifier : public ICommandNotifier
 {
 public:
-	CommandNotifier(CComm& comm)
-		: _comm(comm)
+	CommandNotifier(DWORD tid)
+        : _tid(tid)
 	{}
 
 private:
 	virtual void OnCommand() override {
-		while (Command* bpCmd = _comm.get_command()) {
-			if (bpCmd->type == CommandType::kErrorMessage) {
-				auto pCmd = static_cast<Command_ErrorMessage*>(bpCmd);
-				fprintf(stderr, "错误：%s(%d).\n", pCmd->what.c_str(), pCmd->code);
-				delete pCmd;
-			}
-			else if (bpCmd->type == CommandType::kUpdateCounter) {
-                int nRead, nWritten, nQueued;
-                _comm.get_counter(&nRead, &nWritten, &nQueued);
-                fprintf(stdout, "状态：接收计数:%u,发送计数:%u,等待发送:%u", nRead, nWritten, nQueued);
-				delete bpCmd;
-			}
-			else {
-				delete bpCmd;
-			}
-		}
+        ::PostThreadMessage(_tid, WM_USER + 0, 0, 0);
 	}
 
 private:
-	CComm& _comm;
+    DWORD _tid;
 };
 
 int main()
 {
 	CComm comm;
 	CComm::s_setting_comm settings;
-	CommandNotifier notifier(comm);
+	CommandNotifier notifier(GetCurrentThreadId());
 
 	comm.open(1);
 
@@ -51,14 +36,26 @@ int main()
 	settings.stopbit = ONESTOPBIT;
 
 	comm.setting_comm(&settings);
-
 	comm.begin_threads();
-
 	comm.set_notifier(&notifier);
 
 	MSG msg;
 	while (::GetMessage(&msg, nullptr, 0, 0)) {
+        if (msg.message == WM_USER + 0) {
+            while (Command* bpCmd = comm.get_command()) {
+                if (bpCmd->type == CommandType::kErrorMessage) {
+                    auto pCmd = static_cast<Command_ErrorMessage*>(bpCmd);
+                    fprintf(stderr, "错误：%s(%d).\n", pCmd->what.c_str(), pCmd->code);
+                }
+                else if (bpCmd->type == CommandType::kUpdateCounter) {
+                    int nRead, nWritten, nQueued;
+                    comm.get_counter(&nRead, &nWritten, &nQueued);
+                    fprintf(stdout, "状态：接收计数:%u,发送计数:%u,等待发送:%u", nRead, nWritten, nQueued);
+                }
 
+                delete bpCmd;
+            }
+        }
 	}
 
 	return 0;
